a5a1539a1a73fb89f17e3ad26dcd8e32258b1adb
Alwyn Sterling <alwynsterling5@gmail.com>
2025-01-24 15:12:35 -0500

    Changed to Vite

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,21 +1,18 @@
-import logo from '../images/logo.png'; // Import the logo image from the relative path for use in the component
+import logo from '../images/logo.png';
 
 const Reservation = () => {
     return (
-        <div className="p-5"> {/*Wrapper div with padding applied*/} 
-            <div className="card mx-auto" style={{width: "400px"}}> {/* Card component centered horizontally with a fixed width*/}
-                <div className="card-body"> {/* Card body for internal content*/}
+        <div className="p-5">
+            <div className="card mx-auto" style={{width: "400px"}}>
+                <div className="card-body">
                     <div className="mb-8 text-center">
-                        <img src={logo} alt="Logo" width="100" height="100" className="bi"/> 
-                        <h5 className="card-title">Make A Reservation Today</h5> 
+                        <img src={logo} alt="Logo" width="100" height="100" className="bi"/>
+                        <h5 className="card-title">Make A Reservation Today</h5>
                     </div>
-                    <form className="row g-3"> 
-                        {/* Each form group below consists of an input field and an associated label */}
-                        
-                        {/* First Name input field */}
-                        <div className="form-floating col-md-6"> 
-                            <input type="text" className="form-control" id="first-name" placeholder="First Name"/> 
-                            <label htmlFor="first-name" className="ms-1"> 
+                    <form className="row g-3">
+                        <div className="form-floating col-md-6">
+                            <input type="text" className="form-control" id="first-name" placeholder="First Name"/>
+                            <label htmlFor="first-name" className="ms-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
                                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
                                 </svg>
@@ -23,10 +20,9 @@ const Reservation = () => {
                             </label>
                         </div>
 
-                        {/* Last Name input field */}
                         <div className="form-floating col-md-6">
-                            <input type="text" className="form-control" id="last-name" placeholder="Last Name"/> 
-                            <label htmlFor="last-name" className="ms-1"> 
+                            <input type="text" className="form-control" id="last-name" placeholder="Last Name"/>
+                            <label htmlFor="last-name" className="ms-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
                                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
                                 </svg>
@@ -34,10 +30,9 @@ const Reservation = () => {
                             </label>
                         </div> 
 
-                        {/* Email input field */}
                         <div className="form-floating col-12">
-                            <input type="email" className="form-control" id="Email" placeholder='name@example.com'/> 
-                            <label htmlFor="Email" className="ms-1"> 
+                            <input type="email" className="form-control" id="Email" placeholder='name@example.com'/>
+                            <label htmlFor="Email" className="ms-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16">
                                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
                                 </svg>
@@ -45,10 +40,9 @@ const Reservation = () => {
                             </label>
                         </div>
 
-                        {/* Telephone input field */}
                         <div className="form-floating col-12">
-                            <input type="text" className="form-control" id="telephone" placeholder="Telephone Number"/> 
-                            <label htmlFor="Telephone" className="ms-1"> 
+                            <input type="text" className="form-control" id="telephone" placeholder="Telephone Number"/>
+                            <label htmlFor="Telephone" className="ms-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-telephone" viewBox="0 0 16 16">
                                     <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
                                 </svg>
@@ -56,9 +50,8 @@ const Reservation = () => {
                             </label>
                         </div>
 
-                        {/* Date input field */}
                         <div className="form-floating col-md-6">
-                            <input type="date" className="form-control" id='date-of-resveration'/> 
+                            <input type="date" className="form-control" id='date-of-resveration'/>
                             <label htmlFor="date" className="ms-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar" viewBox="0 0 16 16">
                                     <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
@@ -67,10 +60,9 @@ const Reservation = () => {
                             </label>
                         </div>
 
-                        {/* Time input field */}
                         <div className="form-floating col-md-6">
-                            <input type="time" className="form-control" id="time-of-resveration"/> 
-                            <label htmlFor="Time" className="ms-1"> 
+                            <input type="time" className="form-control" id="time-of-resveration"/>
+                            <label htmlFor="Time" className="ms-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-alarm" viewBox="0 0 16 16">
                                     <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9z"/>
                                     <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1zm1.038 3.018a6 6 0 0 1 .924 0 6 6 0 1 1-.924 0M0 3.5c0 .753.333 1.429.86 1.887A8.04 8.04 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5M13.5 1c-.753 0-1.429.333-1.887.86a8.04 8.04 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1"/>
@@ -79,9 +71,8 @@ const Reservation = () => {
                             </label>
                         </div>
 
-                        {/* Number of guests input field */}
                         <div className="form-floating col-12">
-                            <input type="number" className="form-control" id="num-guest" placeholder='num-guest'/> 
+                            <input type="number" className="form-control" id="num-guest" placeholder='num-guest'/>
                             <label htmlFor="number" className="ms-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-people-fill" viewBox="0 0 16 16">
                                     <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
@@ -90,10 +81,9 @@ const Reservation = () => {
                             </label>
                         </div>
 
-                        {/* Special request input field */}
                         <div className="form-floating col-12">
-                            <input type="text" className="form-control" id="text" placeholder='special-request'/> 
-                            <label htmlFor="text" className="ms-1"> 
+                            <input type="text" className="form-control" id="text" placeholder='special-request'/>
+                            <label htmlFor="text" className="ms-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pen" viewBox="0 0 16 16">
                                     <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
                                 </svg>
@@ -101,8 +91,7 @@ const Reservation = () => {
                             </label>
                         </div>
 
-                        {/* Submit button */}
-                        <button type="submit" className="btn btn-primary w-100 my-2">Make A Reservation</button> 
+                        <button type="submit" className="btn btn-primary w-100 my-2">Make A Reservation</button>
                     </form>
                 </div>
             </div>
@@ -110,4 +99,5 @@ const Reservation = () => {
     );
 };
 
-export default Reservation; // Exporting the component for use in other parts of the application
+export default Reservation;
+  
